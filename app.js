f6eb227578736153b69ee5bb97520775d414688f
Ryan <16924710+pleomorph@users.noreply.github.com>
2019-03-11 20:08:04 -0400

    Add bcrypt hash and salt

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
@@ -23,8 +24,7 @@ const userSchema = new mongoose.Schema ({
   password: String
 });
 
-// Add encryption plugin and secret string from .env
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
+
 
 // Setup Model
 const User = mongoose.model("User", userSchema);
@@ -45,17 +45,20 @@ app.get("/register", function(req,res){
 });
 
 app.post("/register", function(req,res){
-  const newUser = new User({
-    email: req.body.username,
-    password: req.body.password
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err){
+      if (err) {
+        console.log(err);
+      }else{
+        res.render("secrets");
+      }
+    });
   });
-  newUser.save(function(err){
-    if (err) {
-      console.log(err);
-    }else{
-      res.render("secrets");
-    }
-  });
+
 });
 
 app.post("/login", function(req, res){
@@ -66,20 +69,18 @@ app.post("/login", function(req, res){
     if(err){
       console.log(err);
     }else {
-      if (foundUser){ // email match
-        if (foundUser.password === password){
-          res.render("secrets");
+      if (foundUser) { // email match
+        bcrypt.compare(password, foundUser.password, function(err, result) {
+          if (result === true) {
+            res.render("secrets");
+          } else { console.log("Password did not match "); }
+        });
         } else { // no password match
           res.render("login",{
             message: "Invalid login"
           });
         }
-      } else { // no email match
-        res.render("login",{
-          message: "Invalid login"
-        });
       }
-    }
   });
 });
 
